caea2a947971fb0aeb4a78e0bd7bdfe9ef8fab34
+ <+>
2022-09-19 15:04:19 -0700

    commit tree 3b557ebdfe11a46d5d03b3f5fc9d7389976b874d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+
+const excitedWelcomeMessage = (`welcome to flatbook, ${currentUser}!`).toUpperCase()
+
+const intial = currentUser.slice(0,1)
+const shortGreeting = `Welcome, ${intial}!`
